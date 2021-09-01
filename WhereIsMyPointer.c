1dc21d703af2d59851c7e48c18111eacf24d56e6
rick <rick@ricksdomein.nl>
2021-09-01 20:14:27 +0200

    update

--- WhereIsMyPointer.c
+++ WhereIsMyPointer.c
@@ -1,7 +1,6 @@
-/* MIT License
+/* GPLv3 License
 #
-# Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
-# Distributed under the terms of the MIT License.
+# Distributed under the terms of the GPLv3 License.
 #
 # */
 
